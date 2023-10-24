49c4893ffeccf20f1ce0eabd97ed861d3791d9dc
mobo <maximiliano.ramosl@usm.cl>
2023-10-24 04:06:38 -0300

    TERMINADOOO

--- CER2/admin.py
+++ CER2/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import Entidad, Comunicado
+
 # Register your models here.
+class autentificacion(admin.ModelAdmin):
+    list_filter = [("publicado_por",admin.RelatedOnlyFieldListFilter),]
+    list_display = ("id","titulo","publicado_por","tipo","fecha_publicacion")
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.publicado_por = request.user
+        super().save_model(request, obj, form, change)
+
+
+
 admin.site.register(Entidad)
-admin.site.register(Comunicado)
+admin.site.register(Comunicado,autentificacion)
--- CER2/views.py
+++ CER2/views.py
@@ -5,13 +5,20 @@ from .models import Entidad,Comunicado
 # Create your views here.
 
 def web(request):
-    comunicados=Comunicado.objects.all().order_by('-fecha_publicacion')
+    comunicados=Comunicado.objects.all()
     entidades=Entidad.objects.all().order_by('-nombre')
-   
+    seleccionada=request.GET.get("Seleccionada")
 
-    data={"comunicados":comunicados,
+    if seleccionada == 'Todos' or seleccionada is None:
+        comunicados = Comunicado.objects.filter(visible=True)
+    else:
+        filtro = Entidad.objects.get(nombre=seleccionada)
+        comunicados = Comunicado.objects.filter(entidad=filtro,visible=True)
+   
+    data={"comunicados":comunicados.order_by('-fecha_publicacion'),
           "entidades":entidades,
-         
+          "selecto":seleccionada,
+          "comunicadosC":Comunicado.objects.filter(visible=True).order_by('-fecha_publicacion'),
     }
 
 
